2b2bde8bdb40311a4d374ed1e034c36fb8d13be3
baomakele <772176062@qq.com>
2021-05-31 20:39:36 +0800

    fas
    fsa

--- Assets/Script/Core/Editor/Package/ProjectBuildService.cs
+++ Assets/Script/Core/Editor/Package/ProjectBuildService.cs
@@ -258,22 +258,22 @@ class ProjectBuildService : Editor
 
         Debug.LogError("path ==========" + path);
 
-//#if UNITY_2017_1_OR_NEWER
-//		BuildPlayerOptions bo = new BuildPlayerOptions();
-//		bo.scenes = GetBuildScenes();
-//		bo.target = BuildTarget.Android;
-//		bo.options = BuildOptions.None;
-//        bo.locationPathName = path;
-		
-//		BuildPipeline.BuildPlayer(bo);
-//#else
-//        BuildOptions option = BuildOptions.None;
-//        if (ApplicationMode == AppMode.Release)
-//        {
-//            option = BuildOptions.Il2CPP;
-//        }
-//        BuildPipeline.BuildPlayer(GetBuildScenes(), path, BuildTarget.Android, option);
-//#endif
+#if UNITY_2017_1_OR_NEWER
+        BuildPlayerOptions bo = new BuildPlayerOptions();
+        bo.scenes = GetBuildScenes();
+        bo.target = BuildTarget.Android;
+        bo.options = BuildOptions.None;
+        bo.locationPathName = path;
+
+        BuildPipeline.BuildPlayer(bo);
+#else
+        BuildOptions option = BuildOptions.None;
+        if (ApplicationMode == AppMode.Release)
+        {
+            option = BuildOptions.Il2CPP;
+        }
+        BuildPipeline.BuildPlayer(GetBuildScenes(), path, BuildTarget.Android, option);
+#endif
 
 
     }
